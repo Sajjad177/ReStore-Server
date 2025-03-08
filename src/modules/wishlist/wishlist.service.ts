5fc019ebc4c57a2eadae896a42d2d92ffbd045ed
sajjad <sajjadsajjad098765@gmail.com>
2025-03-09 00:46:16 +0600

    wishlist added bug fixed

--- src/modules/wishlist/wishlist.service.ts
+++ src/modules/wishlist/wishlist.service.ts
@@ -2,12 +2,19 @@ import { IWishlist } from "./wishlist.interface";
 import Wishlist from "./wishlist.schema";
 
 const addToWishListInDB = async (payload: IWishlist, userId: string) => {
-  const result = await Wishlist.create({ ...payload, userID: userId });
+  const result = await Wishlist.findOneAndUpdate(
+    { userID: userId },
+    { $addToSet: { listings: payload.listings } },
+    { new: true, upsert: true }
+  );
+
   return result;
 };
 
 const getWishListFromDB = async (userId: string) => {
-  const result = await Wishlist.findOne({ userID: userId });
+  const result = await Wishlist.findOne({ userID: userId }).populate(
+    "listings"
+  );
   return result;
 };
 
